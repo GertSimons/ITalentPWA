1d6963d11f807872040a1e19d68128aa3591e3b4
GertSimons <gertsimons@hotmail.com>
2019-06-16 12:10:18 +0200

    SW

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {SwPush, SwUpdate} from '@angular/service-worker';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ITalentApp';
+
+  constructor(private swUpdate: SwUpdate, private swPush: SwPush) {
+  }
+
+  ngOnInit(): void {
+    if (this.swUpdate.isEnabled) {
+      this.swUpdate.available.subscribe(() => {
+        if (confirm('uw app heeft een update, wilt u deze vernieuwen?')) {
+          window.location.reload();
+        }
+      });
+    }
+  }
 }
+
